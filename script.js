ef04562ab4ec34d17365770ea4fe2418bfce4607
Ava Scherocman <31900548+avascherocman@users.noreply.github.com>
2018-07-05 18:10:07 -0400

    add beginning of cursorBlink function, add logo highlights (no alignment)

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 
-let time = new Date().getTime();
+
+
+
+
+function cursorBlink(){
+  if (document.getElementById("vl").style.visibility =true){
+  document.getElementById("vl").style.visibility=false;
+  }else{
+    document.getElementById("vl").style.visibility=true;
+  }
+}
 
 function typeWriter(i,txt,speed) {
   document.getElementById("typeWriter").innerHTML += txt.charAt(i);
   setTimeout(function() { typeWriter(i+1,txt, speed) }, speed);
-  time = new Date().getTime();
+  cursorBlink();
 }
 
-typeWriter(0,"ava scherocman",100);
-/*let newTime = ((new Date().getTime())-time);
-
-while (newTime < 10000) {
-  newTime = (new Date().getTime()-time);
-}
-
-typeWriter(0,"programmer",100);*/
-
+typeWriter(0,"ava scherocman",1000)
