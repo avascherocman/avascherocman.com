b38536e44275729af88655e0e613a7b4966efc19
Ava Scherocman <31900548+avascherocman@users.noreply.github.com>
2018-07-04 21:30:51 -0400

    add description, fix javascript typing

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 
+let time = new Date().getTime();
+
 function typeWriter(i,txt,speed) {
   document.getElementById("typeWriter").innerHTML += txt.charAt(i);
   setTimeout(function() { typeWriter(i+1,txt, speed) }, speed);
+  time = new Date().getTime();
 }
 
 typeWriter(0,"ava scherocman",100);
+/*let newTime = ((new Date().getTime())-time);
+
+while (newTime < 10000) {
+  newTime = (new Date().getTime()-time);
+}
+
+typeWriter(0,"programmer",100);*/
+
