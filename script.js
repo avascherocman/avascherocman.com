9120cc7b631a8d510060c7623799bec4147dc7a3
Ava Scherocman <31900548+avascherocman@users.noreply.github.com>
2018-07-05 22:06:15 -0400

    add favicons

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-
-
-
-
-
-function cursorBlink(){
-  if (document.getElementById("vl").style.visibility =true){
-  document.getElementById("vl").style.visibility=false;
-  }else{
-    document.getElementById("vl").style.visibility=true;
-  }
-}
+/*function caretBlink(){
+  if (document.getElementById("caret-blink").style.visibility = visible) {
+    document.getElementById("caret-blink").style.visibility=hidden;
+    window.alert("yeet!");
+  } else {
+    window.alert("nope!");
+    document.getElementById("caret-blink").style.visibility=visible;
+}*/
 
 function typeWriter(i,txt,speed) {
   if(i < txt.length) {
     document.getElementById("typeWriter").innerHTML += txt.charAt(i);
     setTimeout(function() { typeWriter(i+1,txt, speed) }, speed);
-    // cursorBlink();
   }
 }
 
+
 typeWriter(0,"ava scherocman",100)
+
+
+/*window.setInterval(function(){
+  caretBlink();
+}, 1000)}*/
